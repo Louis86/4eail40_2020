cfccea537c2c44223418393ca9b35f46eb464ca7
Louis86 <louishuguesa@gmail.com>
2020-10-15 00:41:22 +0200

    Update stringer.go

--- exercises/exo1/stringer.go
+++ exercises/exo1/stringer.go
@@ -1,8 +1,37 @@
 package main
 
-import "fmt"
+import ( "fmt"
+         "math"
+	)
 
 // Implement types Rectangle, Circle and Shape
+type Shape interface {
+   Area() float64
+	 fmt.Stringer
+}
+
+type Rectangle struct{
+   width ,length  float64
+}
+
+type Circle struct{
+   radius float64
+}
+
+func (r Rectangle) Area() float64{
+   return r.length * r.width
+}
+func (c Circle) Area()float64{
+	return 3.14*math.Pow(c.radius,2)
+}
+
+func (r Rectangle) String() string{
+   return fmt.Sprintf("Square of width %.f and length %.f", r.width, r.length)
+}
+
+func (c Circle) String() string{
+   return fmt.Sprintf("Circle of radius %.f", c.radius)
+}
 
 func main() {
 	r := &Rectangle{2, 3}
