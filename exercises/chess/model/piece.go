41cd84b87b5e5548a830641d26dbc9ce74336d8f
Louis86 <louishuguesa@gmail.com>
2020-10-15 23:01:30 +0200

    Update piece.go

--- exercises/chess/model/piece.go
+++ exercises/chess/model/piece.go
@@ -1,3 +1,37 @@
 package model
 
 //Implement game pieces here
+
+
+
+type Piece struct {
+    Name string
+    Color string
+    Move()
+
+}
+
+type Pawn struct {
+Piece
+
+}
+
+type Knight struct {
+Piece
+}
+
+type Rook struct {
+Piece
+}
+
+type Bishop struct {
+Piece
+}
+
+type Queen struct {
+Piece
+}
+
+type King struct {
+Piece
+}
